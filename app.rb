91eb9cdf647577933b8a24b1b3c9848e0f5930cb
Brett Haymaker <brett.haymaker@gmail.com>
2015-05-22 16:27:44 -0500

    Confirms OAuth works with returned results

--- app.rb
+++ app.rb
@@ -8,19 +8,28 @@ Bundler.require()
 # helpers
 
 # Routes
-
 get '/' do
 
   erb :index
 
 end
 
-
 get '/api/tweets' do
-  content_type :json
 
-  client.search("blackhawks", result_type: "recent").take(3).each do |tweet|
-    puts tweet
+
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = "U4YqfF929d4poJvOjWQVbQRgr"
+    config.consumer_secret     = "N9XDHk65IioxOTWFYNBooxRnkWfi6nwAUc67PzA4p6fzETbLVK"
+    config.access_token        = "2398529317-S056Pkh7heukHXQxgkfYaAUGlL7yVpHbWMhcz9y"
+    config.access_token_secret = "fGlyd110GlZvKsEItCO5UZmZN3fyBpoO0gTevyP4cHfJo"
   end
 
+  content_type :json
+  tweets = []
+  client.search('blackhawks', result_type: "mixed").take(5).each do |tweet|
+
+     tweets.push({message: tweet.text, retweet: tweet.retweet_count})
+
+  end
+  tweets.to_json
 end
